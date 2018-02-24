d37dfde53220a7ec3021f8ec6698d2a9203b8f61
sosika <hewjang@gmail.com>
2018-02-24 13:34:36 -0700

    secondpush

--- server.js
+++ server.js
@@ -78,6 +78,8 @@ app.post("/api/new", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body-parser middleware
   var newcharacter = req.body;
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
   newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
 
   console.log(newcharacter);
